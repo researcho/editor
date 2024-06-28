f9f43992b5222804a43a677d0e9dfe00f7a46bd9
Mark Wylde <me@markwylde.com>
2024-06-28 22:16:25 +0100

    chore: save old versions

--- .github/workflows/deployConfig.js
+++ .github/workflows/deployConfig.js
@@ -1,3 +1,7 @@
+import fs from 'fs';
+
+const version = fs.readFileSync('./.VERSION', 'utf8').trim();
+
 export default {
   tasks: [{
     hostname: process.env.FTP_HOSTNAME,
@@ -5,5 +9,11 @@ export default {
     password: process.env.FTP_PASSWORD,
     source: './dist',
     destination: ''
+  }, {
+    hostname: process.env.FTP_HOSTNAME,
+    username: process.env.FTP_USERNAME,
+    password: process.env.FTP_PASSWORD,
+    source: './dist',
+    destination: 'v' + version
   }]
 }
