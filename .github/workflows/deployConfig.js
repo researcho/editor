c0b9cde536cc8671cc5ff378016b90daab435d19
Mark Wylde <me@markwylde.com>
2024-06-28 21:56:23 +0100

    chore: fix auto release

--- .github/workflows/deployConfig.js
+++ .github/workflows/deployConfig.js
@@ -1,11 +1,8 @@
-import info from '../../package.json' assert { type: 'json' };
-
 export default {
   tasks: [{
     hostname: process.env.FTP_HOSTNAME,
     username: process.env.FTP_USERNAME,
     password: process.env.FTP_PASSWORD,
-    source: './dist',
-    destination: 'v' + info.version
+    source: './dist'
   }]
 }
